93efc1f796e640f2ca23ba1d567d4f748998a6f5
Pachamaltese <mvargas@dcc.uchile.cl>
2019-11-29 14:15:52 -0300

    fixed tests

--- tests/testthat/test-descriptive_statistics.R
+++ tests/testthat/test-descriptive_statistics.R
@@ -2,11 +2,21 @@ context("testthat.R")
 
 test_that("media_agrupada works properly", {
   r14 <- leer_casen(system.file(package = "casen", "extdata", "casen_2017_los_rios.zip"))
-  est <- estadistica_descriptiva(r14, "ytotcorh", c("comuna", "sexo"), "expc", 0.7)
-  expect_is(est$media, "data.frame")
-  expect_output(str(est$media), "7 variables")
-  expect_is(est$mediana, "data.frame")
-  expect_output(str(est$mediana), "7 variables")
-  expect_is(est$cuantiles, "data.frame")
-  expect_output(str(est$cuantiles), "7 variables")
+  est <- media_agrupada(r14, "ytotcorh", "comuna", "expc")
+  expect_is(est, "data.frame")
+  expect_output(str(est), "5 variables")
+})
+
+test_that("mediana_agrupada works properly", {
+  r14 <- leer_casen(system.file(package = "casen", "extdata", "casen_2017_los_rios.zip"))
+  est <- mediana_agrupada(r14, "ytotcorh", "comuna", "expc")
+  expect_is(est, "data.frame")
+  expect_output(str(est), "5 variables")
+})
+
+test_that("percentiles_agrupados works properly", {
+  r14 <- leer_casen(system.file(package = "casen", "extdata", "casen_2017_los_rios.zip"))
+  est <- percentiles_agrupados(r14, "ytotcorh", c("comuna","sexo"), "expc", 0.7)
+  expect_is(est, "data.frame")
+  expect_output(str(est), "7 variables")
 })
