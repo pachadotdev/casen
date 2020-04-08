18904046eb242d18bf6926edf8856249ecfa75cf
Kirill Müller <krlmlr+r@mailbox.org>
2020-04-08 06:21:48 +0200

    Use verify_output()

--- tests/testthat/test-descriptive_statistics.R
+++ tests/testthat/test-descriptive_statistics.R
@@ -1,22 +1,15 @@
 context("testthat.R")
 
-test_that("media_agrupada works properly", {
+verify_output("agrupada.txt", {
+  "# media_argrupada()"
   cd <- configuracion_disenio(casen_2017_los_rios, "ytotcorh", c("comuna", "sexo"), "expc")
-  est <- media_agrupada(cd)
-  expect_is(est, "data.frame")
-  expect_output(str(est), "7 variables")
-})
+  media_agrupada(cd)
 
-test_that("mediana_agrupada works properly", {
+  "# mediana_argrupada()"
   cd <- configuracion_disenio(casen_2017_los_rios, "ytotcorh", c("comuna", "sexo"), "expc")
-  est <- mediana_agrupada(cd)
-  expect_is(est, "data.frame")
-  expect_output(str(est), "7 variables")
-})
+  mediana_agrupada(cd)
 
-test_that("percentiles_agrupados works properly", {
+  "# percentiles_agrupados()"
   cd <- configuracion_disenio(casen_2017_los_rios, "ytotcorh", c("comuna", "sexo"), "expc")
-  est <- percentiles_agrupados(cd, 0.7)
-  expect_is(est, "data.frame")
-  expect_output(str(est), "7 variables")
+  percentiles_agrupados(cd, 0.7)
 })
